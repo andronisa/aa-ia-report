f3a994be028477541d75d4417663872448734bef
Antonios Andronis <aa3e15@soton.ac.uk>
2016-01-08 00:02:49 +0000

    getting some stuff from the logs

--- app/log_parser.py
+++ app/log_parser.py
@@ -5,15 +5,74 @@ from config import GAME_LOG_PATH, PLOT_PATH
 
 class LogParser:
     def __init__(self):
-        pass
+        self.day = 0
+        self.day_failed = None
+        self.status = True
+        self.games = []
+        self.agents = {}
+        self.line = ''
 
     @staticmethod
     def get_game_logs():
         return glob.glob(os.path.join(GAME_LOG_PATH, "*.txt"))
 
+    def add_agent_to_list(self):
+        agent_name = self.line.split(":")[0]
+        if agent_name not in self.agents:
+            self.agents[agent_name] = {
+                'status': True,
+                'campaigns': []
+            }
+
     def parse_logs(self):
-        for game_log in self.get_game_logs():
-            print(game_log)
+        for logfile in self.get_game_logs():
+            self.parse_log(logfile)
+
+    def set_day(self):
+        self.day = int(self.line.split(":")[1].strip().split(" ")[0])
+
+    def set_bank_balance(self):
+        for agent, stats in self.agents.iteritems():
+            if agent in self.line and self.agents[agent]['status']:
+                self.agents[agent]['bank_balance'] = round(float(self.line.split(":")[1].strip()), 2)
+
+    def set_campaign_report(self):
+        for agent, stats in self.agents.iteritems():
+            if agent in self.line and self.agents[agent]['status']:
+                campaign = int(
+                        self.line.split(":")[1].split("Targeted Impressions")[0].strip().split(" ")[0].replace("#", ""))
+                if campaign not in self.agents[agent]['campaigns']:
+                    self.agents[agent]['campaigns'].append(campaign)
+
+    def set_quality(self):
+        for agent, stats in self.agents.iteritems():
+            if agent in self.line:
+                quality_rating = round(float(self.line.split(":")[1].strip()), 2)
+                if quality_rating < 0.31 and self.agents[agent]['status'] is True:
+                    self.agents[agent]['day_reached'] = self.day
+                    self.agents[agent]['status'] = False
+                elif quality_rating > 0.31 and self.agents[agent]['status'] is False:
+                    self.agents[agent]['status'] = True
+                    self.agents[agent]['day_reached'] = self.day
+                elif quality_rating > 0.31 and self.agents[agent]['status'] is True:
+                    self.agents[agent]['day_reached'] = self.day
+                self.agents[agent]['quality_rating'] = quality_rating
+
+    def parse_log(self, logfile):
+        with open(logfile, 'r') as game_log:
+            # game_name = logfile.split("_")[1].split(".")[0]
+            for i, self.line in enumerate(game_log):
+                if i in range(7, 15):
+                    self.add_agent_to_list()
+                if 'Next day' in self.line:
+                    self.set_day()
+                if 'Bank balance:' in self.line:
+                    self.set_bank_balance()
+                if 'Quality rating:' in self.line:
+                    self.set_quality()
+                if 'Campaign report:' in self.line:
+                    self.set_campaign_report()
+            self.games.append(self.agents)
 
 
 class GameLog:
@@ -24,3 +83,6 @@ class GameLog:
 if __name__ == '__main__':
     log_parser = LogParser()
     log_parser.parse_logs()
+    for game in log_parser.games:
+        for agent, stats in game.iteritems():
+            print(agent, stats)
