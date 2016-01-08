c84b77e07c05995a5298a435741983aa2c447cc1
Antonios Andronis <aa3e15@soton.ac.uk>
2016-01-08 04:32:49 +0000

    changes

--- app/log_parser.py
+++ app/log_parser.py
@@ -1,5 +1,8 @@
+from __future__ import division
+
 import os
 import glob
+import re
 from config import GAME_LOG_PATH, PLOT_PATH
 
 
@@ -12,8 +15,15 @@ class LogParser:
         self.agents = {}
         self.line = ''
 
+    # GAME_LOG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'game_logs'))
+
     @staticmethod
-    def get_game_logs():
+    def numerical_sort(afilename):
+        nondigits = re.compile("\D")
+        return int(nondigits.sub("", afilename))
+
+    def get_game_logs(self):
+        # return sorted(glob.glob(os.path.join(GAME_LOG_PATH, "*.txt")), key=self.numerical_sort)
         return glob.glob(os.path.join(GAME_LOG_PATH, "*.txt"))
 
     def add_agent_to_list(self):
@@ -21,68 +31,111 @@ class LogParser:
         if agent_name not in self.agents:
             self.agents[agent_name] = {
                 'status': True,
-                'campaigns': []
+                'campaigns': [],
+                'balances': [],
+                'ucs_levels': [],
+                'daily_cost': [],
+                'qualities': [],
             }
 
     def parse_logs(self):
+        game_counter = 0
+        sets_counter = 1
         for logfile in self.get_game_logs():
-            self.parse_log(logfile)
+            game_counter += 1
+
+            if game_counter % 4 == 0:
+                sets_counter += 1
+            self.parse_log(logfile, sets_counter)
 
     def set_day(self):
         self.day = int(self.line.split(":")[1].strip().split(" ")[0])
 
     def set_bank_balance(self):
         for agent, stats in self.agents.iteritems():
-            if agent in self.line and self.agents[agent]['status']:
-                self.agents[agent]['bank_balance'] = round(float(self.line.split(":")[1].strip()), 2)
+            if agent in self.line:
+                self.agents[agent]['balances'].append(round(float(self.line.split(":")[1].strip()), 2))
 
-    def set_campaign_report(self):
+    def set_campaign(self):
         for agent, stats in self.agents.iteritems():
-            if agent in self.line and self.agents[agent]['status']:
+            if agent in self.line:
                 campaign = int(
                         self.line.split(":")[1].split("Targeted Impressions")[0].strip().split(" ")[0].replace("#", ""))
                 if campaign not in self.agents[agent]['campaigns']:
                     self.agents[agent]['campaigns'].append(campaign)
 
+    def set_ucs_level(self):
+        for agent, stats in self.agents.iteritems():
+            if agent in self.line:
+                self.agents[agent]['ucs_levels'].append(round(float(self.line.split(":")[1].strip()), 2))
+
+    def set_daily_cost_per_imp(self):
+        for agent, stats in self.agents.iteritems():
+            if agent in self.line:
+                imps_arr = self.line.split("Targeted Impressions: ")[1].split("	")[0].strip().split(".")
+                daily_imps = float(imps_arr[0][0:5] + "." + imps_arr[1][0:5])
+                daily_cost = float(self.line.split("Cost: ")[1].strip()[0:6])
+
+                if daily_imps != 0.0 and daily_cost != 0.0:
+                    cost_per_imp = round(daily_cost / daily_imps, 4)
+                    if cost_per_imp != 0.0:
+                        self.agents[agent]['daily_cost'].append(cost_per_imp)
+
     def set_quality(self):
         for agent, stats in self.agents.iteritems():
             if agent in self.line:
                 quality_rating = round(float(self.line.split(":")[1].strip()), 2)
-                if quality_rating < 0.31 and self.agents[agent]['status'] is True:
-                    self.agents[agent]['day_reached'] = self.day
-                    self.agents[agent]['status'] = False
-                elif quality_rating > 0.31 and self.agents[agent]['status'] is False:
-                    self.agents[agent]['status'] = True
-                    self.agents[agent]['day_reached'] = self.day
-                elif quality_rating > 0.31 and self.agents[agent]['status'] is True:
-                    self.agents[agent]['day_reached'] = self.day
-                self.agents[agent]['quality_rating'] = quality_rating
+                if self.agents[agent]['status'] is True:
+                    if quality_rating < 0.31:
+                        self.agents[agent]['day_reached'] = self.day
+                        self.agents[agent]['status'] = False
+                        self.agents[agent]['qualities'].append(quality_rating)
+                    else:
+                        self.agents[agent]['day_reached'] = self.day
+                        self.agents[agent]['qualities'].append(quality_rating)
+                else:
+                    if quality_rating < 0.31 and ((self.day - self.agents[agent]['day_reached']) < 15):
+                        self.agents[agent]['qualities'].append(quality_rating)
+                    else:
+                        continue
 
-    def parse_log(self, logfile):
+    def parse_log(self, logfile, sets_counter):
+        self.day = 0
+        self.agents = {}
+        self.line = ''
+
         with open(logfile, 'r') as game_log:
-            # game_name = logfile.split("_")[1].split(".")[0]
+            # # game_name = logfile.split("_")[1].split(".")[0]
             for i, self.line in enumerate(game_log):
                 if i in range(7, 15):
                     self.add_agent_to_list()
                 if 'Next day' in self.line:
                     self.set_day()
-                if 'Bank balance:' in self.line:
+                elif 'Bank balance:' in self.line:
                     self.set_bank_balance()
-                if 'Quality rating:' in self.line:
+                elif 'Quality rating:' in self.line:
                     self.set_quality()
-                if 'Campaign report:' in self.line:
-                    self.set_campaign_report()
+                elif 'UCS level:' in self.line:
+                    self.set_ucs_level()
+                elif 'Campaign report:' in self.line:
+                    self.set_campaign()
+                    self.set_daily_cost_per_imp()
             self.games.append(self.agents)
-
-
-class GameLog:
-    def __init__(self):
-        pass
 
 
 if __name__ == '__main__':
     log_parser = LogParser()
     log_parser.parse_logs()
+    counter = 0
     for game in log_parser.games:
-        for agent, stats in game.iteritems():
-            print(agent, stats)
+        counter += 1
+        for agent_name, agent_stats in game.iteritems():
+            print(
+                agent_name,
+                agent_stats['balances'],
+                # agent_stats['campaigns'],
+                # agent_stats['balances'],
+                # agent_stats['ucs_levels'],
+                # agent_stats['daily_cost'],
+                # agent_stats['qualities'],
+            )
--- app/plotter.py
+++ app/plotter.py
@@ -5,6 +5,8 @@ import sys
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+from log_parser import LogParser
+from pylab import *
 
 LOG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'logs'))
 
@@ -13,260 +15,278 @@ class Plotter:
     def __init__(self):
         pass
 
-    def plot_top_ten_categories(self):
-        def percentage(part, whole):
-            return int(100 * float(part) / float(whole))
+        # def plot_top_ten_categories(self):
+        #     def percentage(part, whole):
+        #         return int(100 * float(part) / float(whole))
+        #
+        #     def calculate_rest_count(categories, whole):
+        #         sum = 0
+        #         for item in categories:
+        #             sum += item['count']
+        #             return sum
+        #
+        #     pipeline = [
+        #         {"$unwind": "$categories"},
+        #         {"$group": {"_id": "$categories", "count": {"$sum": 1}}},
+        #         {"$sort": {"count": -1}},
+        #         {"$limit": 9}
+        #     ]
+        #
+        #     col_name = 'review_category'
+        #
+        #     fracs = []
+        #     category_labels = []
+        #     should_explode = True
+        #     should_append_rest = True
+        #     expl = []
+        #
+        #     for category in categories:
+        #         if should_explode:
+        #             expl.append(0.1)
+        #             should_explode = False
+        #         else:
+        #             expl.append(0)
+        #
+        #         review_count = category['count']
+        #
+        #         if review_count < rest_count and should_append_rest:
+        #             fracs.append(percentage(rest_count, total_categories_no))
+        #             category_labels.append("Others")
+        #             expl.append(0)
+        #
+        #             should_append_rest = False
+        #
+        #         fracs.append(percentage(review_count, total_categories_no))
+        #         category_labels.append(category['_id'])
+        #
+        #     labels = tuple(category_labels)
+        #     explode = tuple(expl)
+        #
+        #     # make a square figure and axes
+        #     figure(1, figsize=(6, 6))
+        #     ax = axes([0.1, 0.1, 0.8, 0.8])
+        #
+        #     pie(fracs, colors=colors, explode=explode, labels=labels,
+        #         autopct='%1.1f%%', shadow=True, startangle=90)
+        #     # The default startangle is 0, which would start
+        #     # the Frogs slice on the x-axis.  With startangle=90,
+        #     # everything is rotated counter-clockwise by 90 degrees,
+        #     # so the plotting starts on the positive y-axis.
+        #     mpl.rcParams['font.size'] = 15.0
+        #     title('Top Business Categories', bbox={'facecolor': '0.8', 'pad': 5})
+        #
+        #     show()
+        #
+        # def plot_top_ten_businesses_of_top_category(self):
+        #     def percentage(part, whole):
+        #         return int(100 * float(part) / float(whole))
+        #
+        #     top_category = self.find_top_category()
+        #     top_ten_business_id_docs = self.find_top_businesses_of_category(top_category, 3)
+        #
+        #     businesses_for_plot = []
+        #     total = 0
+        #     for item in top_ten_business_id_docs:
+        #         business_id = item['_id']
+        #         bus_count = item['count']
+        #         total += bus_count
+        #
+        #         business_name = self.query.find_one('business', [('business_id', business_id)], ['name'])['name']
+        #         businesses_for_plot.append((bus_count, business_name))
+        #
+        #     category_labels = []
+        #     should_explode = True
+        #     counts = []
+        #     expl = []
+        #
+        #     leength = len(businesses_for_plot)
+        #     colors = cm.Set1(np.arange(len(businesses_for_plot)) / leength)
+        #
+        #     for count, name in businesses_for_plot:
+        #         if should_explode:
+        #             expl.append(0.1)
+        #             should_explode = False
+        #         else:
+        #             expl.append(0)
+        #
+        #         counts.append(count)
+        #
+        #         category_labels.append(name)
+        #
+        #     labels = tuple(category_labels)
+        #     business_counts = tuple(counts)
+        #     ind = np.arange(3)  # the x locations for the groups
+        #     width = 0.35  # bar width
+        #
+        #     fig, ax = plt.subplots()
+        #
+        #     rects1 = ax.bar(ind, business_counts,  # data
+        #                     width,  # bar width
+        #                     color=colors,  # bar colour
+        #                     error_kw={'ecolor': 'Tomato',  # error-bars colour
+        #                               'linewidth': 2})  # error-bar width
+        #
+        #     axes = plt.gca()
+        #     axes.set_ylim([0, 5000])  # y-axis bounds
+        #
+        #     ax.set_ylabel('Reviews')
+        #     ax.set_title('Most Popular Businesses')
+        #     ax.set_xticks(ind + width / 2)
+        #     ax.set_xticklabels(labels)
+        #
+        #     rect_tuple = tuple(rects1)
+        #
+        #     ax.legend(rect_tuple, labels)
+        #     params = {'legend.fontsize': 25,
+        #               'legend.linewidth': 3}
+        #     mpl.rcParams.update(params)
+        #
+        #     def autolabel(rects):
+        #         for rect in rects:
+        #             height = rect.get_height()
+        #             ax.text(rect.get_x() + rect.get_width() / 2., 1.05 * height,
+        #                     '%d' % int(height),
+        #                     ha='center',  # vertical alignment
+        #                     va='bottom'  # horizontal alignment
+        #                     )
+        #
+        #     autolabel(rects1)
+        #
+        #     plt.show()
+        #
+        # def plot_bus(self, result):
+        #     category_labels = []
+        #     counts = []
+        #     colors = cm.Set1(np.arange(len(result)) / len(result))
+        #
+        #     for name, count in result:
+        #         counts.append(count)
+        #         name_arr = name.split(" ")
+        #         if len(name_arr) > 1:
+        #             category_labels.append(name_arr[0])
+        #         else:
+        #             category_labels.append(name)
+        #
+        #     labels = tuple(category_labels)
+        #     business_counts = tuple(counts)
+        #     ind = np.arange(len(result))  # the x locations for the groups
+        #     width = 0.35  # bar width
+        #
+        #     fig, ax = plt.subplots()
+        #
+        #     rects1 = ax.bar(ind, business_counts,  # data
+        #                     width,  # bar width
+        #                     color=colors,  # bar colour
+        #                     error_kw={'ecolor': 'Tomato',  # error-bars colour
+        #                               'linewidth': 2})  # error-bar width
+        #
+        #     axes = plt.gca()
+        #     axes.set_ylim([0, 3])  # y-axis bounds
+        #
+        #     ax.set_ylabel('Occurences')
+        #     ax.set_title('Complained keywords')
+        #     ax.set_xticks(ind + width / 2)
+        #     ax.set_xticklabels(labels)
+        #
+        #     rect_tuple = tuple(rects1)
+        #
+        #     ax.legend(rect_tuple, labels)
+        #
+        #     def autolabel(rects):
+        #         for rect in rects:
+        #             height = rect.get_height()
+        #             ax.text(rect.get_x() + rect.get_width() / 2., 1.05 * height,
+        #                     '%d' % int(height),
+        #                     ha='center',  # vertical alignment
+        #                     va='bottom'  # horizontal alignment
+        #                     )
+        #
+        #     autolabel(rects1)
+        #
+        #     plt.show()
+        #
 
-        def calculate_rest_count(categories, whole):
-            sum = 0
-            for item in categories:
-                sum += item['count']
-                return sum
+    def bar_plot_qualities(self, results):
+        total_agent_qualities = {}
+        agent_labels = []
+        agent_qualities = []
 
-        pipeline = [
-            {"$unwind": "$categories"},
-            {"$group": {"_id": "$categories", "count": {"$sum": 1}}},
-            {"$sort": {"count": -1}},
-            {"$limit": 9}
-        ]
+        for game in results:
+            for agent, stats in game.iteritems():
+                total_agent_qualities[agent]['qualities'].append(stats['qualities'])
 
-        col_name = 'review_category'
+        for agentname, qualities in total_agent_qualities.iteritems():
+            agent_labels.append(agentname)
+            agent_qualities.append(sum(qualities) / len(qualities))
 
-        fracs = []
-        category_labels = []
-        should_explode = True
-        should_append_rest = True
-        expl = []
+        colors = cm.Set1(np.arange(len(total_agent_qualities)) / len(total_agent_qualities))
 
-        for category in categories:
-            if should_explode:
-                expl.append(0.1)
-                should_explode = False
-            else:
-                expl.append(0)
-
-            review_count = category['count']
-
-            if review_count < rest_count and should_append_rest:
-                fracs.append(percentage(rest_count, total_categories_no))
-                category_labels.append("Others")
-                expl.append(0)
-
-                should_append_rest = False
-
-            fracs.append(percentage(review_count, total_categories_no))
-            category_labels.append(category['_id'])
-
-        labels = tuple(category_labels)
-        explode = tuple(expl)
-
-        # make a square figure and axes
-        figure(1, figsize=(6, 6))
-        ax = axes([0.1, 0.1, 0.8, 0.8])
-
-        pie(fracs, colors=colors, explode=explode, labels=labels,
-            autopct='%1.1f%%', shadow=True, startangle=90)
-        # The default startangle is 0, which would start
-        # the Frogs slice on the x-axis.  With startangle=90,
-        # everything is rotated counter-clockwise by 90 degrees,
-        # so the plotting starts on the positive y-axis.
-        mpl.rcParams['font.size'] = 15.0
-        title('Top Business Categories', bbox={'facecolor': '0.8', 'pad': 5})
-
-        show()
-
-    def plot_top_ten_businesses_of_top_category(self):
-        def percentage(part, whole):
-            return int(100 * float(part) / float(whole))
-
-        top_category = self.find_top_category()
-        top_ten_business_id_docs = self.find_top_businesses_of_category(top_category, 3)
-
-        businesses_for_plot = []
-        total = 0
-        for item in top_ten_business_id_docs:
-            business_id = item['_id']
-            bus_count = item['count']
-            total += bus_count
-
-            business_name = self.query.find_one('business', [('business_id', business_id)], ['name'])['name']
-            businesses_for_plot.append((bus_count, business_name))
-
-        category_labels = []
-        should_explode = True
-        counts = []
-        expl = []
-
-        leength = len(businesses_for_plot)
-        colors = cm.Set1(np.arange(len(businesses_for_plot)) / leength)
-
-        for count, name in businesses_for_plot:
-            if should_explode:
-                expl.append(0.1)
-                should_explode = False
-            else:
-                expl.append(0)
-
-            counts.append(count)
-
-            category_labels.append(name)
-
-        labels = tuple(category_labels)
-        business_counts = tuple(counts)
-        ind = np.arange(3)  # the x locations for the groups
+        labels = tuple(agent_labels)
+        qualities = tuple(agent_qualities)
+        ind = np.arange(len(total_agent_qualities))  # the x locations for the groups
         width = 0.35  # bar width
 
         fig, ax = plt.subplots()
 
-        rects1 = ax.bar(ind, business_counts,  # data
+        rects1 = ax.bar(ind, qualities,  # data
                         width,  # bar width
                         color=colors,  # bar colour
                         error_kw={'ecolor': 'Tomato',  # error-bars colour
-                                  'linewidth': 2})  # error-bar width
+                                  'linewidth': 1})  # error-bar width
 
         axes = plt.gca()
-        axes.set_ylim([0, 5000])  # y-axis bounds
+        axes.set_ylim([0, 1.5])  # y-axis bounds
 
-        ax.set_ylabel('Reviews')
-        ax.set_title('Most Popular Businesses')
+        ax.set_ylabel('Quality rating')
+        ax.set_title('Agents Quality')
         ax.set_xticks(ind + width / 2)
         ax.set_xticklabels(labels)
 
         rect_tuple = tuple(rects1)
 
         ax.legend(rect_tuple, labels)
-        params = {'legend.fontsize': 25,
-                  'legend.linewidth': 3}
-        mpl.rcParams.update(params)
 
         def autolabel(rects):
             for rect in rects:
                 height = rect.get_height()
+                print(rect.get_x() + rect.get_width() / 2.)
                 ax.text(rect.get_x() + rect.get_width() / 2., 1.05 * height,
-                        '%d' % int(height),
+                        '%g' % height,
                         ha='center',  # vertical alignment
                         va='bottom'  # horizontal alignment
                         )
 
         autolabel(rects1)
-
-        plt.show()
-
-    def plot_bus(self, result):
-        category_labels = []
-        counts = []
-        colors = cm.Set1(np.arange(len(result)) / len(result))
-
-        for name, count in result:
-            counts.append(count)
-            name_arr = name.split(" ")
-            if len(name_arr) > 1:
-                category_labels.append(name_arr[0])
-            else:
-                category_labels.append(name)
-
-        labels = tuple(category_labels)
-        business_counts = tuple(counts)
-        ind = np.arange(len(result))  # the x locations for the groups
-        width = 0.35  # bar width
-
-        fig, ax = plt.subplots()
-
-        rects1 = ax.bar(ind, business_counts,  # data
-                        width,  # bar width
-                        color=colors,  # bar colour
-                        error_kw={'ecolor': 'Tomato',  # error-bars colour
-                                  'linewidth': 2})  # error-bar width
-
-        axes = plt.gca()
-        axes.set_ylim([0, 3])  # y-axis bounds
-
-        ax.set_ylabel('Occurences')
-        ax.set_title('Complained keywords')
-        ax.set_xticks(ind + width / 2)
-        ax.set_xticklabels(labels)
-
-        rect_tuple = tuple(rects1)
-
-        ax.legend(rect_tuple, labels)
-
-        def autolabel(rects):
-            for rect in rects:
-                height = rect.get_height()
-                ax.text(rect.get_x() + rect.get_width() / 2., 1.05 * height,
-                        '%d' % int(height),
-                        ha='center',  # vertical alignment
-                        va='bottom'  # horizontal alignment
-                        )
-
-        autolabel(rects1)
-
-        plt.show()
-
-    def bar_plot(result, title):
-        OX = [
-            'MinMax',
-            'Alpha Beta',
-            'Tie'
-        ]
-
-        mm = result['mm_wins']
-        ab = result['ab_wins']
-        ties = result['ties']
-
-        OY = [mm, ab, ties]
-
-        fig = plt.figure()
-
-        width = .20
-        ind = np.arange(len(OY))
-        barlist = plt.bar(ind, OY, align='center')
-        plt.xticks(ind + width / 2, OX)
-
-        barlist[0].set_color('r')
-        barlist[1].set_color('b')
-        barlist[2].set_color('g')
-        plt.legend((barlist[0], barlist[1], barlist[2]),
-                   ('MinMax Wins: ' + str(mm), 'AlphaBeta Wins: ' + str(ab), 'Ties: ' + str(ties)))
-
-        plt.xlabel('Result')
-        plt.ylabel('Wins Count')
-        plt.title(title)
-        plt.show()
-
-    def plot_times(result, limit=None, title=None):
-        bfs = result['bfs']
-        id_dfs = result['id_dfs']
-        a_star = result['a_star']
-
-        bfs_average = sum(bfs) / len(bfs)
-        id_dfs_average = sum(id_dfs) / len(id_dfs)
-        a_star_average = sum(a_star) / len(a_star)
-
-        lines = plt.plot(bfs, 'xr-', id_dfs, 'xb-', a_star, 'xg-', linewidth=3)
-
-        plt.legend((lines[0], lines[1], lines[2]),
-                   ('BFS: ' + str(bfs_average) + " seconds",
-                    'IDDFS: ' + str(id_dfs_average) + " seconds",
-                    'A*: ' + str(a_star_average) + " seconds"),
-                   loc=1)
-
-        plt.xlabel('Time(seconds)')
-        plt.ylabel('Times tried')
-        if title:
-            plt.title(title)
-
-        if limit:
-            x1, x2, y1, y2 = plt.axis()
-
-            plt.axis((x1, x2, 0, limit))
 
         plt.show()
 
 
 if __name__ == '__main__':
+    parser = LogParser()
+    results = parser.parse_logs()
+    print(results)
+
     plotter = Plotter()
+    plotter.bar_plot_qualities(results)
+
+    # for game in parser.games:
+    #     plotter.bar_plot_qualities(game)
+    #     for agent_name, agent_stats in game.iteritems():
+    #         pass
+    # plottable_qualities[agent_name] = agent_stats['qualities']
+    # print(
+    #     agent_name,
+    #     agent_stats['day_reached'],
+    #     agent_stats['campaigns'],
+    #     agent_stats['balances'],
+    #     agent_stats['ucs_levels'],
+    #     agent_stats['daily_cost'],
+    #     agent_stats['qualities'],
+    # )
+    #
+    # plotter.bar_plot(agent_stats['balances'])
+    # plotter = get_results()
     # plotter.plot_buses()
 
     # plotter.plot_top_ten_categories()
-    plotter.plot_top_ten_businesses_of_top_category()
+    # plotter.plot_top_ten_businesses_of_top_category()
